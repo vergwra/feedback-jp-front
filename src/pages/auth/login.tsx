f7ed4fff526902ed3f94f1f662bd08c9ba6eadf6
Marina Vergara <vergwraaa@gmail.com>
2023-06-02 14:58:47 -0300

    login page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -24,14 +24,26 @@ function Login() {
     }
 
     return (
-        <div className="mainContainer">
+        <>
+        
             <Header></Header>
+        <div className="middle">
             <div>
-                <input  onChange={e=> setEmail(e.target.value)} placeholder="email"></input>
-                <input onChange={e=> setPassword(e.target.value)} placeholder="senha"></input>
-                <button onClick={handleLogin}>login</button>
+                <div className='backgroundLogin'>
+                    <div className='loginTitle'>
+                        <h1>Login</h1>
+                        <p>Entre com seu e-mail e sua senha para acessar a plataforma.</p>
+                    </div>
+                    <input  onChange={e=> setEmail(e.target.value)} placeholder="email"></input>
+                    <input onChange={e=> setPassword(e.target.value)} placeholder="senha"></input>
+                    <div>
+                        <a>Esqueceu sua senha?</a>
+                        <button onClick={handleLogin}>login</button>
+                    </div>
+                </div>
             </div>
         </div>
+        </>
     );
 }
 
